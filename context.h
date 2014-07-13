bacfe73a6d54af7e3270fe5c9ff178b0f7c79aae
arfarf <arfarf@sharma>
2014-07-14 00:23:48 +0200

    Code cleaning...

---
+++ context.h
@@ -0,0 +1,1 @@
+void myKiSystemCall64ASM(void);
--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <ntddk.h>
 #include <wdm.h>
-
-//TODO .h
-extern void myKiSystemCall64ASM(void);
+#include "context.h"
 
 #define MAX_ACTIVE_CPU 16
 #define LSTAR_MSR 0xC0000082
 
 typedef VOID(*KISYSTEMCALL64)(); 
+
 KISYSTEMCALL64 origKiSystemCall64[MAX_ACTIVE_CPU];
 KISYSTEMCALL64 myPrologKiSystemCall64;
-
 ULONG active_processor_count;
 
 VOID prologKiSystemCall64(){
@@ -69,11 +67,9 @@ VOID UnhookAllLSTAR(){
 
 VOID DriverUnload(PDRIVER_OBJECT pDriverObject) {
 	DbgPrint("\nFill my heart with song\n");
-	
 	DisplayAllLSTAR();
 	UnhookAllLSTAR();
 	DisplayAllLSTAR();
-	
 	DbgPrint("Let me sing forever more\n");  
 }
 
@@ -81,24 +77,25 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING RegistryPath)
 	UINT16 current_processor;
 	
 	myPrologKiSystemCall64 = prologKiSystemCall64;
+	active_processor_count = KeQueryActiveProcessorCount(NULL);
+	
+	if(active_processor_count == 0
+	|| active_processor_count > MAX_ACTIVE_CPU){
+		DbgPrint("Too many processors !\n");
+		return STATUS_UNSUCCESSFUL;
+	}
+
 	
 	for(current_processor=0; current_processor<MAX_ACTIVE_CPU; current_processor++){
 		origKiSystemCall64[current_processor] = NULL;
 	}
 	
 	DbgPrint("Fly me to the moon...\n");
-	active_processor_count = KeQueryActiveProcessorCount( NULL );
-	if(active_processor_count == 0
-	|| active_processor_count > MAX_ACTIVE_CPU){
-		DbgPrint("Too many processors !\n");
-		return STATUS_UNSUCCESSFUL;
-	}
-	
 	DisplayAllLSTAR();
 	HookAllLSTAR();
 	DisplayAllLSTAR();
-	
 	DbgPrint("And let me play among the LSTAR !\n");
+	
 	pDriverObject->DriverUnload = DriverUnload; 
 	return STATUS_SUCCESS;
 }  
