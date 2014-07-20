a3ee06a4a016de1ccb16e392f295a50d1ccce3bf
arfarf <arfarf@sharma>
2014-07-20 11:16:11 +0200

    Working even with patchguard activated

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 #define MAX_ACTIVE_CPU 16
 #define LSTAR_MSR 0xC0000082
 
+#define SECRET_MSR	0x01234567
+#define BYPASS_ACTIVATION	0xDEADDEADDEADDEAD
+#define BYPASS_DESACTIVATION 0xC0DEC0DEC0DEC0DE
+
 typedef VOID(*KISYSTEMCALL64)(); 
 
 KISYSTEMCALL64 origKiSystemCall64[MAX_ACTIVE_CPU];
 KISYSTEMCALL64 myPrologKiSystemCall64;
 ULONG active_processor_count;
 UINT64 syscall_number;
+UINT64 proceed_syscall;
 
 VOID prologKiSystemCall64(){
-	DbgPrint("In other words, please be true %16x\n", syscall_number);
+	//DbgPrint("In other words, please be true %16x\n", syscall_number);
+	proceed_syscall++;
 }
 
 VOID DisplayAllLSTAR(){
@@ -37,6 +43,11 @@ VOID HookAllLSTAR(){
 	GROUP_AFFINITY old_affinity, new_affinity;
 	UINT16 current_processor;
 	
+	try{
+		__writemsr(SECRET_MSR, BYPASS_ACTIVATION);
+	}except (EXCEPTION_EXECUTE_HANDLER) {
+	}
+	
 	for(current_processor=0; current_processor<active_processor_count; current_processor++){
 		memset(&new_affinity, 0, sizeof(GROUP_AFFINITY));
 		memset(&old_affinity, 0, sizeof(GROUP_AFFINITY));
@@ -52,6 +63,11 @@ VOID HookAllLSTAR(){
 VOID UnhookAllLSTAR(){
 	GROUP_AFFINITY old_affinity, new_affinity;
 	UINT16 current_processor;
+	
+	try{
+		__writemsr(SECRET_MSR, BYPASS_DESACTIVATION);
+	}except (EXCEPTION_EXECUTE_HANDLER) {
+	}
 	
 	for(current_processor=0; current_processor<active_processor_count; current_processor++){
 		if(origKiSystemCall64[current_processor]){
@@ -71,7 +87,8 @@ VOID DriverUnload(PDRIVER_OBJECT pDriverObject) {
 	DisplayAllLSTAR();
 	UnhookAllLSTAR();
 	DisplayAllLSTAR();
-	DbgPrint("Let me sing forever more\n");  
+	DbgPrint("Let me sing forever more\n");
+	DbgPrint("Proceed syscall %p\n", proceed_syscall);
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING RegistryPath){
@@ -79,6 +96,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING RegistryPath)
 	
 	myPrologKiSystemCall64 = prologKiSystemCall64;
 	active_processor_count = KeQueryActiveProcessorCount(NULL);
+	proceed_syscall = 0;
 	
 	if(active_processor_count == 0
 	|| active_processor_count > MAX_ACTIVE_CPU){
